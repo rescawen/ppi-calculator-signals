cdb7f0fc79fa49e991498ab34606f6025c86ad49
Wenlei Dai <resca.wen@gmail.com>
2023-04-03 10:34:24 +0300

    Updated DisplayData with 2 more properties
    Trying out atomic commit philosophy #1

--- src/interfaces/DisplayData.ts
+++ src/interfaces/DisplayData.ts
@@ -1,14 +1,16 @@
 export interface DisplayData {
   brand: string;
+  model: string;
   horizontal_resolution: number;
   vertical_resolution: number;
+  diagonal: number;
 }
 
 export const mockDisplayData: DisplayData[] = [
-  { brand: "Apple", horizontal_resolution: 1920, vertical_resolution: 1080 },
-  { brand: "Apple", horizontal_resolution: 4096, vertical_resolution: 2304 },
-  { brand: "LG", horizontal_resolution: 3840, vertical_resolution: 2160 },
-  { brand: "LG", horizontal_resolution: 4096, vertical_resolution: 2304 },
-  { brand: "LG", horizontal_resolution: 1920, vertical_resolution: 1080 },
-  { brand: "Samsung", horizontal_resolution: 1920, vertical_resolution: 1080 },
+  { brand: "Apple", model: "display", horizontal_resolution: 1920, vertical_resolution: 1080, diagonal: 24  },
+  { brand: "Apple", model: "imac 21.5", horizontal_resolution: 4096, vertical_resolution: 2304, diagonal: 21.5 },
+  { brand: "LG", model: "ultrafine 4k", horizontal_resolution: 3840, vertical_resolution: 2160, diagonal: 27 },
+  { brand: "LG", model: "ultrafine 4k+", horizontal_resolution: 4096, vertical_resolution: 2304, diagonal: 21.5 },
+  { brand: "LG", model: "gaming", horizontal_resolution: 1920, vertical_resolution: 1080, diagonal: 24 },
+  { brand: "Samsung",model: "gaming", horizontal_resolution: 1920, vertical_resolution: 1080, diagonal: 24 },
 ];
